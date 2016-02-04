7c2a1cc5d786f433c23086ec589db7e1b396d6e4
Anton Ostapenko <olsnod@gmail.com>
2016-02-05 00:10:04 +0500

    add test for vk.users.get_username_by_id_or_none

--- tests/vk/test_users.py
+++ tests/vk/test_users.py
@@ -4,7 +4,7 @@ import mock
 import pytest
 import requests
 
-from vk.users import get_number_of_followers_or_none
+from vk.users import get_number_of_followers_or_none, get_username_by_id_or_none
 
 
 @pytest.mark.parametrize('user_id, number_of_followers_in_response', [(1, 100500), (2, 0)])
@@ -35,3 +35,27 @@ def test_get_number_of_followers_or_none_with_error():
         __, kwargs = mock_of_requests_get.call_args_list[0]
         params_kwarg = kwargs['params']
         assert params_kwarg['user_id'] == user_id
+
+
+@pytest.mark.parametrize('user_id, response_username', [(1, "Foo Bar"), (100, "Dead Beef")])
+def test_get_username_by_id_or_none(user_id, response_username):
+    """Тестируем логику get_username_by_id_or_none
+    :param user_id: int request
+    :param response_username: str response
+    """
+    name = response_username.split()
+    json_return_value = {'response': [{"id": user_id, 'first_name': name[0], 'last_name': name[1]}]}
+    with mock.patch('requests.get') as mock_of_requests_get:
+        mock_of_requests_get.return_value = mock_of_response = mock.Mock()
+        mock_of_response.json.return_value = json_return_value
+
+        username = get_username_by_id_or_none(user_id=user_id)
+        assert username == response_username
+
+
+def test_get_username_by_id_or_none_with_error():
+    """Тестируем get_username_by_id_or_none при возникновении ошибки"""
+    user_id = 42
+    with mock.patch('requests.get') as mock_of_requests_get:
+        mock_of_requests_get.side_effect = requests.exceptions.HTTPError()
+        assert get_username_by_id_or_none(user_id) is None
